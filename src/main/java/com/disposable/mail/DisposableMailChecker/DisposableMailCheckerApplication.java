7db4975a0750c3555f277d6d442c9ece2dfcd7c5
Bikas Mahapatra <b.mahapatra@axelor.com>
2024-02-19 18:34:51 +0530

    Init App

---
+++ src/main/java/com/disposable/mail/DisposableMailChecker/DisposableMailCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.disposable.mail.DisposableMailChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisposableMailCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisposableMailCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/disposable/mail/DisposableMailChecker/DisposableMailCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disposable.mail.DisposableMailChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisposableMailCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
